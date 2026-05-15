ac31d867891d6dee78474d7c7fee3725f9918cb0
Link4Electronics <link4electronics@gmail.com>
2026-05-15 03:48:54 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_QT=1
 export QT_DIR=qt6
 
 # Deploy dependencies
-quick-sharun /usr/bin/kstars /usr/share/kstars /usr/share/sounds/KDE-*.ogg
+quick-sharun /usr/bin/kstars /usr/share/kstars /usr/share/sounds/KDE-*.ogg /usr/lib/libsecret*.so*
 
 # Additional changes can be done in between here
 
